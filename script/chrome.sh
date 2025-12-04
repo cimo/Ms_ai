646b2814a2a2412b872342d1c5ee3eebf3f03608
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-12-04 20:02:35 +0900

    update

--- script/chrome.sh
+++ script/chrome.sh
@@ -2,4 +2,4 @@
 
 eval "$(dbus-launch --auto-syntax)"
 
-google-chrome --headless --no-sandbox --shm-size=2g --disable-gpu --no-first-run --no-default-browser-check
+google-chrome --headless --no-sandbox --shm-size=2g --disable-dev-shm-usage --disable-gpu --no-first-run --no-default-browser-check
