e90b65b38afbad9f2b004727ab0608f28cda49bc
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-12-05 18:03:49 +0900

    update

--- mcp/example/src/Main.ts
+++ mcp/example/src/Main.ts
@@ -2,19 +2,41 @@ import { FastMCP } from "fastmcp";
 import { z } from "zod";
 
 const server = new FastMCP({
-    name: "Example",
-    version: "1.0.0"
+    name: "Server example",
+    version: "1.0.0",
+    roots: {
+        enabled: false
+    }
 });
 
 server.addTool({
-    name: "sum",
+    name: "cimo-tool",
     description: "Sum 2 numbers.",
     parameters: z.object({
         a: z.number(),
         b: z.number()
     }),
-    execute: async (argument: { a: number; b: number }) => {
-        return String(argument.a + argument.b);
+    execute: async (argument: { a: number; b: number }, { reportProgress }) => {
+        await reportProgress({
+            progress: 0,
+            total: 100
+        });
+
+        //...
+
+        await reportProgress({
+            progress: 100,
+            total: 100
+        });
+
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: String(argument.a + argument.b)
+                }
+            ]
+        };
     }
 });
 
