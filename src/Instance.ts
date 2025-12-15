3ed270250d5b21582acaf3089c9523193f6a52d4
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-12-15 19:27:41 +0900

    update

---
+++ src/Instance.ts
@@ -0,0 +1,32 @@
+import { Cr } from "@cimo/request/dist/src/Main.js";
+
+// Source
+import * as HelperSrc from "./HelperSrc.js";
+
+export const api = new Cr(`${HelperSrc.URL_ENDPOINT || ""}`, HelperSrc.REQUEST_TIMEOUT, HelperSrc.REQUEST_IS_ENCODED);
+
+api.setRequestInterceptor((config: RequestInit) => {
+    return requestLogic(config);
+});
+
+api.setResponseInterceptor((response: Response) => {
+    return responseLogic(response);
+});
+
+const requestLogic = (config: RequestInit): RequestInit => {
+    return {
+        ...config,
+        headers: {
+            ...config.headers
+        },
+        credentials: "include"
+    };
+};
+
+const responseLogic = (response: Response) => {
+    if (response.status === 403 || response.status === 500) {
+        HelperSrc.writeLog("error", response.status.toString());
+    }
+
+    return response;
+};
---
+++ src/model/LmStudio.ts
@@ -0,0 +1,10 @@
+export interface IresponseModel {
+    data: [
+        {
+            id: string;
+            object: string;
+            owned_by: string;
+        }
+    ];
+    object: string;
+}
