1c3f4f35ec320682a85c26642116e52bd39610c8
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-01-22 15:12:35 +0900

    update

--- mcp_server/src/tool/Math/Sum.ts
+++ mcp_server/src/tool/Math/Sum.ts
@@ -1,27 +1,3 @@
-import { z } from "zod";
-import type { Context } from "fastmcp";
-
-// Source
-import * as modelMain from "../../model/Main.js";
-
-const parameter = z.object({
-    a: z.number(),
-    b: z.number()
-});
-
-const tool = {
-    name: "tool_sum",
-    description: "Sum 2 numbers.",
-    parameters: parameter,
-    execute: async (argument: z.infer<typeof parameter>, { reportProgress }: Context<modelMain.Auth>) => {
-        await reportProgress({ progress: 0, total: 100 });
-
-        const result = argument.a + argument.b;
-
-        await reportProgress({ progress: 100, total: 100 });
-
-        return String(result);
-    }
+export const sum = (a: number, b: number): number => {
+    return a + b;
 };
-
-export default tool;
