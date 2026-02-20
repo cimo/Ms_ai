ad6fc174906d69c9304f8849a60a37583900ed81
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-02-20 21:52:02 +0900

    update

--- src/model/LmStudio.ts
+++ src/model/LmStudio.ts
@@ -1,4 +1,4 @@
-export interface IresponseModel {
+export interface IapiModel {
     data: [
         {
             id: string;
@@ -7,4 +7,44 @@ export interface IresponseModel {
         }
     ];
     object: string;
+}
+
+export interface IapiResponseItem {
+    tool_call_id: string;
+    type: string;
+    name: string;
+    arguments: string;
+    output: string;
+    content: [
+        {
+            type: string;
+            text: string;
+        }
+    ];
+}
+
+export interface IapiResponse {
+    type: string;
+    output_index: number;
+    sequence_number: number;
+    delta: string;
+    item: IapiResponseItem;
+    response: {
+        id: string;
+        message: string;
+    };
+    error: {
+        code: string;
+        message: string;
+        param: string;
+        type: string;
+    };
+}
+
+export interface ItoolTask {
+    [key: string]: unknown;
+    stepList: {
+        action: string;
+        argumentList: [Record<string, unknown>];
+    }[];
 }
