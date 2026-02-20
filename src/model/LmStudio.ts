95b93dc0fa4a7ce29960eb16b035b6516fd18160
cimo <simone.dagostino@gmail.com>
2026-02-21 00:52:50 +0900

    update

--- src/model/LmStudio.ts
+++ src/model/LmStudio.ts
@@ -10,11 +10,6 @@ export interface IapiModel {
 }
 
 export interface IapiResponseItem {
-    tool_call_id: string;
-    type: string;
-    name: string;
-    arguments: string;
-    output: string;
     content: [
         {
             type: string;
@@ -26,25 +21,14 @@ export interface IapiResponseItem {
 export interface IapiResponse {
     type: string;
     output_index: number;
-    sequence_number: number;
-    delta: string;
     item: IapiResponseItem;
-    response: {
-        id: string;
-        message: string;
-    };
-    error: {
-        code: string;
-        message: string;
-        param: string;
-        type: string;
-    };
 }
 
 export interface ItoolTask {
-    [key: string]: unknown;
-    stepList: {
-        action: string;
-        argumentList: [Record<string, unknown>];
-    }[];
+    stepList: [
+        {
+            action: string;
+            argumentObject: Record<string, string>;
+        }
+    ];
 }
